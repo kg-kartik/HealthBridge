a40f757623041cf0990d47bb419bdbbfd487deb8
amanjagdev <amankumarjagdev@gmail.com>
2020-11-08 14:13:32 +0530

    Completed with Styling and services

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,9 @@ import Footer from "./components/Footer";
 
 import PrivateRoute from "./components/PrivateRoute";
 import Appointment from "./pages/services/Appointment";
+import Medicine from "./pages/services/Medicine";
+import Heart from "./pages/services/Heart";
+import Pneumonia from "./pages/services/Pneumonia";
 import Cancer from "./pages/services/Cancer";
 import Covid from "./pages/services/Covid";
 import DashboardDoc from "./pages/DashboardDoc";
@@ -38,9 +41,24 @@ const App = () => {
 						component={Cancer}
 					/>
 					<PrivateRoute
+						path="/dashboard/pneumonia"
+						exact
+						component={Pneumonia}
+					/>
+					<PrivateRoute
+						path="/dashboard/medicine"
+						exact
+						component={Medicine}
+					/>
+					<PrivateRoute
 						path="/dashboard/doctor"
 						exact
 						component={Appointment}
+					/>
+					<PrivateRoute
+						path="/dashboard/heart"
+						exact
+						component={Heart}
 					/>
 					<PrivateRoute
 						path="/dashboard/covid"
